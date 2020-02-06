7f368789b0010d5c75755b6c2af3a09ace3ab6de
SK Dutta <skd@officeclip.com>
2020-02-05 19:35:19 -0500

    Added the nuget package

--- OAuth2.Lib/Properties/AssemblyInfo.cs
+++ OAuth2.Lib/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OAuth2.Lib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OfficeClip.OpenSource.OAuth2.Lib")]
+[assembly: AssemblyDescription("A Wrapper over Google OAuth2")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OAuth2.Lib")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("OfficeClip")]
+[assembly: AssemblyProduct("OfficeClip.OpenSource.OAuth2.Lib")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
