43dde688c8819288fda19c13e0e38f8add01d5e6
Nagesh Kulkarni <nagesh@officeclip.com>
2022-02-09 19:32:27 +0530

    Added contact group source file.

---
+++ OAuth2.Google/Google/People/ContactsGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OfficeClip.OpenSource.OAuth2.Services.Google.People
+{
+    public class ContactsGroup
+    {
+        
+
+    }
+}
